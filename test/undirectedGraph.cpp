239364fa7cc0e9568fd0ea9e4882bd599ab24efe
KriaPia <kristoffer.berg8@gmail.com>
2025-10-21 15:35:27 +0200

    Implemented undirected graph test cases

--- test/undirectedGraph.cpp
+++ test/undirectedGraph.cpp
@@ -4,127 +4,130 @@
 using Edge = Basis::Edge;
 using type = Basis::GraphType;
 
-class DirectedGraphTest: public testing::Test {
-    protected:
-    Basis::Graph<type::directed, int> graph;
+// TODO: write tests for undirected graph 
 
-    DirectedGraphTest(): graph() {}
+class UndirectedGraphTest: public testing::Test {
+    protected:
+    Basis::Graph<type::undirected, int> graph;
+
+    UndirectedGraphTest(): graph() {}
 };
 
-class PopulatedDirectedGraphTest: public testing::Test {
+class PopulatedUndirectedGraphTest: public testing::Test {
     protected:
     Basis::Graph<type::directed, int> graph {};
 
-    PopulatedDirectedGraphTest() {
+    PopulatedUndirectedGraphTest() {
         graph.addEdge(Edge{.from = 0, .to = 1});
         graph.addEdge(Edge{.from = 1, .to = 2});
         graph.addEdge(Edge{.from = 2, .to = 0});
     }
 };
 
-TEST_F(DirectedGraphTest, AddNonexistentEdge) {
+TEST_F(UndirectedGraphTest, AddNonexistentEdge) {
     const auto toAdd {Edge{.from = 0, .to = 1}};
-    const auto toAddToo {Edge{.from = 1, .to = 2}};
+    const auto toAddReverse {Edge{.from = toAdd.to, .to = toAdd.from}};
     graph.addEdge(toAdd);
-    graph.addEdge(toAddToo);
 
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
-    ASSERT_TRUE(graph.doesVertexExist(toAddToo.from));
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
-    ASSERT_TRUE(graph.doesVertexExist(toAddToo.to));
-    ASSERT_TRUE(graph.doesEdgeExist(toAdd));
-    ASSERT_TRUE(graph.doesEdgeExist(toAddToo));
-}
-
-TEST_F(DirectedGraphTest, AddEdgeFromExistingVertex) {
-    const auto existingEdge {Edge{.from = 0, .to = 2}};
-    graph.addEdge(existingEdge);
-    ASSERT_TRUE(graph.doesVertexExist(existingEdge.from));
-    ASSERT_TRUE(graph.doesVertexExist(existingEdge.to));
-    ASSERT_TRUE(graph.doesEdgeExist(existingEdge));
-
-    const auto toAdd {Edge{.from = 0, .to = 1}};
-    graph.addEdge(toAdd);
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
-    ASSERT_TRUE(graph.doesEdgeExist(toAdd));
-}
-
-TEST_F(DirectedGraphTest, AddEdgeToExistingVertex) {
-    const auto existingEdge {Edge{.from = 0, .to = 2}};
-    graph.addEdge(existingEdge);
-    ASSERT_TRUE(graph.doesVertexExist(existingEdge.from));
-    ASSERT_TRUE(graph.doesVertexExist(existingEdge.to));
-    ASSERT_TRUE(graph.doesEdgeExist(existingEdge));
-
-    const auto toAdd {Edge{.from = 0, .to = 1}};
-    graph.addEdge(toAdd);
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
-    ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
-    ASSERT_TRUE(graph.doesEdgeExist(toAdd));
-}
-
-TEST_F(DirectedGraphTest, AddExistingEdge) {
-    const auto toAdd {Edge{.from = 0, .to = 1}};
-    graph.addEdge(toAdd);
-    graph.addEdge(toAdd);
 
     ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
     ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
     ASSERT_TRUE(graph.doesEdgeExist(toAdd));
+    ASSERT_TRUE(graph.doesEdgeExist(toAddReverse));
     ASSERT_EQ(graph.getVertexCount(), 2);
     ASSERT_EQ(graph.getEdgeCount(), 1);
 }
 
-TEST_F(DirectedGraphTest, VertexCountEmpty) {
+TEST_F(UndirectedGraphTest, AddEdgeFromExistingVertex) {
+    const auto existingEdge {Edge{.from = 0, .to = 2}};
+    const auto existingEdgeReverse {Edge{.from = existingEdge.to, .to = existingEdge.from}};
+    graph.addEdge(existingEdge);
+    ASSERT_TRUE(graph.doesVertexExist(existingEdge.from));
+    ASSERT_TRUE(graph.doesVertexExist(existingEdge.to));
+    ASSERT_TRUE(graph.doesEdgeExist(existingEdge));
+    ASSERT_TRUE(graph.doesEdgeExist(existingEdgeReverse));
+    ASSERT_EQ(graph.getEdgeCount(), 1);
+
+    const auto toAdd {Edge{.from = 0, .to = 1}};
+    const auto toAddReverse {Edge{.from = toAdd.to, .to = toAdd.from}};
+    graph.addEdge(toAdd);
+    ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
+    ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
+    ASSERT_TRUE(graph.doesEdgeExist(toAdd));
+    ASSERT_TRUE(graph.doesEdgeExist(toAddReverse));
+    ASSERT_EQ(graph.getEdgeCount(), 2);
+    ASSERT_EQ(graph.getVertexCount(), 3);
+}
+
+TEST_F(UndirectedGraphTest, AddExistingEdge) {
+    const auto toAdd {Edge{.from = 0, .to = 1}};
+    const auto toAddReverse {Edge{.from = toAdd.to, .to = toAdd.from}};
+    graph.addEdge(toAdd);
+    graph.addEdge(toAddReverse);
+    graph.addEdge(toAdd);
+
+    ASSERT_TRUE(graph.doesVertexExist(toAdd.from));
+    ASSERT_TRUE(graph.doesVertexExist(toAdd.to));
+    ASSERT_TRUE(graph.doesEdgeExist(toAdd));
+    ASSERT_TRUE(graph.doesEdgeExist(toAddReverse));
+    ASSERT_EQ(graph.getVertexCount(), 2);
+    ASSERT_EQ(graph.getEdgeCount(), 1);
+}
+
+TEST_F(UndirectedGraphTest, VertexCountEmpty) {
     ASSERT_EQ(graph.getVertexCount(), 0);
 }
 
-TEST_F(DirectedGraphTest, VertexCountNonEmpty) {
+TEST_F(UndirectedGraphTest, VertexCountNonEmpty) {
     graph.addEdge(Edge{.from = 0, .to = 1});
     ASSERT_EQ(graph.getVertexCount(), 2);
 }
 
-TEST_F(DirectedGraphTest, DoesEdgeExist) {
+TEST_F(UndirectedGraphTest, DoesEdgeExist) {
     graph.addEdge(Edge{.from = 0, .to = 1});
     ASSERT_EQ(graph.getEdgeCount(), 1);
     ASSERT_TRUE(graph.doesEdgeExist(Edge{.from = 0, .to = 1}));
+    ASSERT_TRUE(graph.doesEdgeExist(Edge{.from = 1, .to = 0}));
 }
 
-TEST_F(DirectedGraphTest, DoesEdgeExistNonexistentEdge) {
+TEST_F(UndirectedGraphTest, DoesEdgeExistNonexistentEdge) {
     ASSERT_EQ(graph.getEdgeCount(), 0);
     ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 0, .to = 1}));
+    ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 1, .to = 0}));
 }
 
-TEST_F(PopulatedDirectedGraphTest, RemoveEdge) {
+TEST_F(PopulatedUndirectedGraphTest, RemoveEdge) {
     const auto previousEdgeCount {graph.getEdgeCount()};
     const auto previousVertexCount {graph.getVertexCount()};
     ASSERT_TRUE(graph.removeEdge(Edge{.from = 2, .to = 0}));
     ASSERT_EQ(graph.getEdgeCount(), previousEdgeCount - 1);
     ASSERT_EQ(graph.getVertexCount(), previousVertexCount);
     ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 2, .to = 0}));
+    ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 0, .to = 2}));
 }
 
-TEST_F(PopulatedDirectedGraphTest, RemoveEdgeTwice) {
+TEST_F(PopulatedUndirectedGraphTest, RemoveEdgeTwice) {
     const auto previousEdgeCount {graph.getEdgeCount()};
     ASSERT_TRUE(graph.removeEdge(Edge{.from = 2, .to = 0}));
-    ASSERT_FALSE(graph.removeEdge(Edge{.from = 2, .to = 0}));
+    ASSERT_FALSE(graph.removeEdge(Edge{.from = 0, .to = 2}));
     ASSERT_EQ(graph.getEdgeCount(), previousEdgeCount - 1);
     ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 2, .to = 0}));
+    ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 0, .to = 2}));
 }
 
-TEST_F(PopulatedDirectedGraphTest, RemoveEdgeReversal) {
-    graph.addEdge(Edge{.from = 0, .to = 2});
+TEST_F(PopulatedUndirectedGraphTest, RemoveEdgeReversal) {
     const auto previousEdgeCount {graph.getEdgeCount()};
-    ASSERT_TRUE(graph.removeEdge(Edge{.from = 2, .to = 0}));
+    ASSERT_TRUE(graph.removeEdge(Edge{.from = 0, .to = 2}));
+
     ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 2, .to = 0}));
-    ASSERT_TRUE(graph.doesEdgeExist(Edge{.from = 0, .to = 2}));
+    ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 0, .to = 2}));
     ASSERT_EQ(graph.getEdgeCount(), previousEdgeCount - 1);
 }
 
-TEST_F(PopulatedDirectedGraphTest, RemoveNonexistentEdge) {
+TEST_F(PopulatedUndirectedGraphTest, RemoveNonexistentEdge) {
     const auto previousEdgeCount {graph.getEdgeCount()};
     ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 0, .to = 3}));
+    ASSERT_FALSE(graph.doesEdgeExist(Edge{.from = 3, .to = 0}));
     ASSERT_FALSE(graph.removeEdge(Edge{.from = 0, .to = 3}));
     ASSERT_EQ(graph.getEdgeCount(), previousEdgeCount);
 }
